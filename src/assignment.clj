162f19aa9e7f0c06fa8eb9700dc44979cc8783c0
deepikabartwal <44020585+deepikabartwal@users.noreply.github.com>
2020-01-06 14:46:05 +0530

    Add get-gcd, get-lcm, calculate-simple-interest and calculate-compound-interest

--- src/assignment.clj
+++ src/assignment.clj
@@ -10,8 +10,23 @@
 
 (defn find-greatest-of-three-numbers [firstNumber secondNumber thirdNumber] (max firstNumber secondNumber thirdNumber))
 
-(defn get-average-of-three-numbers [firstNum secondNum thirdNum] (float (/ ( + firstNum secondNum thirdNum) 3)))
+(defn get-average-of-three-numbers [firstNum secondNum thirdNum] (float (/ (+ firstNum secondNum thirdNum) 3)))
 
 (defn sum-of-n-numbers [limit] (/ (* limit (inc limit)) 2))
 
+(defn get-gcd [num1 num2]
+  ( loop [ divisor (min num1 num2)
+          dividend (max num1 num2)]
+    (if (zero? divisor)
+      dividend
+      (recur (rem dividend divisor) divisor)
+      )
+    )
+  )
+
+(defn get-lcm [num1 num2] (/ (* num1 num2) (get-gcd num1 num2)))
+
+(defn calculate-simple-interest [principle rate time-period] (/ (* principle rate time-period) 100))
+
+(defn calculate-compound-interest [principle rate time-period] (- (* principle (Math/pow (+ 1 (/ rate 100)) time-period)) principle))
 (println (sum-of-n-numbers 100))
